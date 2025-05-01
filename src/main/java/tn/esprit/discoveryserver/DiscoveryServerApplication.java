f14393d28b14451259aa2243398ff4b38fbb9cbb
GHARBI Abdelhafidh <130412760+AbdelhafidhGHARBI@users.noreply.github.com>
2025-05-02 00:12:43 +0100

    ✨ Configuration du Discovery Server
    - Mise à jour de la classe DiscoveryServerApplication pour activer Eureka Server
    - Configuration du fichier application.yml avec les paramètres d'enregistrement et de découverte de services (port, nom d'application, etc.)

--- src/main/java/tn/esprit/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/tn/esprit/discoveryserver/DiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package tn.esprit.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer  // Active le serveur Eureka
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
